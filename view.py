7dbcb092ccd57ed21513937b2ae84af5a066bfd6
Giuseppe Averta <g.averta3@gmail.com>
2025-03-24 10:13:23 +0100

    fix

--- view.py
+++ view.py
@@ -16,9 +16,16 @@ class View(object):
         self._txtOutTMax = ft.TextField(label="T Max", disabled=True, width=200, value=self._controller.getTMax())
         self._txtOutT = ft.TextField(label="T rimanenti", disabled=True, width=200)
 
-        self._txtIn = ft.TextField(label="Tentativo", width=200)
-        self._btnReset = ft.ElevatedButton(text="Nuova Partita", width=200, on_click=self._controller.reset)
-        self._btnPlay = ft.ElevatedButton(text="Gioca", width=200, on_click=self._controller.play)
+        self._txtIn = ft.TextField(label="Tentativo",
+                                   width=200,
+                                   disabled=True)
+        self._btnReset = ft.ElevatedButton(text="Nuova Partita",
+                                           width=200,
+                                           on_click=self._controller.reset)
+        self._btnPlay = ft.ElevatedButton(text="Gioca",
+                                          width=200,
+                                          on_click=self._controller.play,
+                                          disabled=True)
 
         self._lv = ft.ListView(expand=True)
 
